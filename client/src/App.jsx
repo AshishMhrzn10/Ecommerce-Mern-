e1107e544eb09a49ff5708a2a43307a75ae2d346
Ashish Maharjan <amaharjan1032@gmail.com>
2021-11-21 09:01:17 +0545

    Cart done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,10 @@ import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 const App = () => {
-  return <Login />;
+  return <Cart />;
 };
 
 export default App;
