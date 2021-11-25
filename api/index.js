1383c8dbd82f986fe48b8c496a4c4f3b1185ca44
Ashish Maharjan <amaharjan1032@gmail.com>
2021-11-25 12:35:26 +0545

    routers setup

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoute = require("./routes/user");
 
 dotenv.config();
 
@@ -13,6 +14,9 @@ mongoose
     .catch((err) => {
         console.log(err);
     });
+
+app.use(express.json());
+app.use("/api/user", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running.");
