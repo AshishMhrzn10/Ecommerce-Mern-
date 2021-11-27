fe5cb4154e002ef8f734ab216f6646cb44f8481f
Ashish Maharjan <amaharjan1032@gmail.com>
2021-11-27 15:52:33 +0545

    Update done

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,4 +1,23 @@
 const router = require('express').Router();
+const User = require('../models/User');
+const { verifyTokenAndAuthorization } = require("./verifyToken");
+const CryptoJS = require('crypto-js');
 
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString();
+    }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true });
+        res.status(200).json(updatedUser);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
