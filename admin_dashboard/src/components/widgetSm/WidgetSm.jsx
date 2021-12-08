d9f9330979b888306b81ff8a4127011da94bfa20
Ashish Maharjan <amaharjan1032@gmail.com>
2021-12-08 13:26:28 +0545

    Fetching elements

--- admin_dashboard/src/components/widgetSm/WidgetSm.jsx
+++ admin_dashboard/src/components/widgetSm/WidgetSm.jsx
@@ -1,94 +1,43 @@
 import "./widgetSm.css";
 import { Visibility } from "@material-ui/icons";
+import { useEffect, useState } from "react";
+import { userRequest } from "../../requestMethods";
 
 export default function WidgetSm() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const getUsers = async () => {
+            try {
+                const res = await userRequest.get("users/?new=true");
+                setUsers(res.data);
+            }
+            catch (err) {
+                console.log(err);
+            }
+        };
+        getUsers();
+    }, []);
     return (
         <div className="widgetSm">
             <span className="widgetSmTitle">New Join Members</span>
             <ul className="widgetSmList">
-                <li className="widgetSmListItem">
-                    <img
-                        src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                        alt=""
-                        className="widgetSmImg"
-                    />
-                    <div className="widgetSmUser">
-                        <span className="widgetSmUsername">Anna Keylor</span>
-                        <span className="widgetSmUserTitle">Software Engineer</span>
-                    </div>
-                    <button class="widgetSmButton">
-                        <Visibility className="widgetSmIcon" />
-                        Display
-                    </button>
-                </li>
-
-                <li className="widgetSmListItem">
-                    <img
-                        src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                        alt=""
-                        className="widgetSmImg"
-                    />
-                    <div className="widgetSmUser">
-                        <span className="widgetSmUsername">Anna Keylor</span>
-                        <span className="widgetSmUserTitle">Software Engineer</span>
-                    </div>
-                    <button class="widgetSmButton">
-                        <Visibility className="widgetSmIcon" />
-                        Display
-                    </button>
-                </li>
-
-
-                <li className="widgetSmListItem">
-                    <img
-                        src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                        alt=""
-                        className="widgetSmImg"
-                    />
-                    <div className="widgetSmUser">
-                        <span className="widgetSmUsername">Anna Keylor</span>
-                        <span className="widgetSmUserTitle">Software Engineer</span>
-                    </div>
-                    <button class="widgetSmButton">
-                        <Visibility className="widgetSmIcon" />
-                        Display
-                    </button>
-                </li>
-
-
-                <li className="widgetSmListItem">
-                    <img
-                        src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                        alt=""
-                        className="widgetSmImg"
-                    />
-                    <div className="widgetSmUser">
-                        <span className="widgetSmUsername">Anna Keylor</span>
-                        <span className="widgetSmUserTitle">Software Engineer</span>
-                    </div>
-                    <button class="widgetSmButton">
-                        <Visibility className="widgetSmIcon" />
-                        Display
-                    </button>
-                </li>
-
-
-                <li className="widgetSmListItem">
-                    <img
-                        src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                        alt=""
-                        className="widgetSmImg"
-                    />
-                    <div className="widgetSmUser">
-                        <span className="widgetSmUsername">Anna Keylor</span>
-                        <span className="widgetSmUserTitle">Software Engineer</span>
-                    </div>
-                    <button class="widgetSmButton">
-                        <Visibility className="widgetSmIcon" />
-                        Display
-                    </button>
-                </li>
-
+                {users.map(user => (
+                    <li className="widgetSmListItem" key={user._id}>
+                        <img
+                            src={user.img || "https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png"}
+                            alt=""
+                            className="widgetSmImg"
+                        />
+                        <div className="widgetSmUser">
+                            <span className="widgetSmUsername">{user.username}</span>
+                        </div>
+                        <button className="widgetSmButton">
+                            <Visibility className="widgetSmIcon" />
+                            Display
+                        </button>
+                    </li>
+                ))}
             </ul>
         </div>
     );
--- api/models/User.js
+++ api/models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
+    img: { type: String },
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", UserSchema);
